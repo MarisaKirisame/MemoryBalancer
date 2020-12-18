32542d7481e37ab712da0417bc280bf65bbc2e43
Marisa Kirisame <lolisa@marisa.moe>
2020-12-18 15:09:36 -0800

    save

--- compile.sh
+++ compile.sh
@@ -6,9 +6,10 @@ cd v8
 ninja -C out.gn/x64.release.sample v8_monolith
 cd ../
 
-cd MemoryBalancer/build
+cd MemoryBalancer
+mkdir -p build
+cd build
 cmake ../
 cmake --build .
-#g++ -std=c++17 -I. -I../v8/include -Ithird_party/json/include hello-world.cc -o hello_world -lv8_monolith -L../v8/out.gn/x64.release.sample/obj/ -pthread  -DV8_COMPRESS_POINTERS
 #systemd-run --scope -p MemoryLimit=300M ./hello_world
 
