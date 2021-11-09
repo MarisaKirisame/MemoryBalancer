333ea6265698757288900c6bd78460736157536f
Marisa Kirisame <lolisa@marisa.moe>
2021-11-09 14:55:07 -0700

    save

--- anal.py
+++ anal.py
@@ -24,10 +24,10 @@ class FrozenDict(collections.Mapping):
         return self._d[key]
 
     def __hash__(self):
-        # It would have been simpler and maybe more obvious to 
+        # It would have been simpler and maybe more obvious to
         # use hash(tuple(sorted(self._d.iteritems()))) from this discussion
-        # so far, but this solution is O(n). I don't know what kind of 
-        # n we are going to run into, but sometimes it's hard to resist the 
+        # so far, but this solution is O(n). I don't know what kind of
+        # n we are going to run into, but sometimes it's hard to resist the
         # urge to optimize when it will gain improved algorithmic performance.
         if self._hash is None:
             hash_ = 0
