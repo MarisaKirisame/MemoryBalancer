661a94ebaafdbde509be4256c236ce4f62a36ddf
Marisa Kirisame <lolisa@marisa.moe>
2021-12-24 01:32:24 -0700

    save

--- src/v8_eval.cpp
+++ src/v8_eval.cpp
@@ -58,6 +58,12 @@ V8_Result run_v8(v8::Platform* platform, const std::vector<std::pair<size_t, std
 
 constexpr bool single = false;
 
+struct Benchmark {
+  std::string directory;
+  std::string name;
+  size_t repeat_time;
+};
+
 void v8_experiment(v8::Platform* platform, const std::vector<char*>& args) {
   cxxopts::Options options("V8 Experiment", "run some experiment from jetstream");
   options.add_options()
@@ -75,38 +81,28 @@ void v8_experiment(v8::Platform* platform, const std::vector<char*>& args) {
   std::string jetstream2_path = browserbench_path + "JetStream2.0/";
   std::string octane_path = jetstream2_path + "Octane/";
   std::string sunspider_path = jetstream1_path + "sunspider/";
-  std::vector<std::pair<std::string, std::string>> jetstream2_js_paths;
+  std::vector<Benchmark> jetstream2_js_paths;
   // js_paths.push_back(octane_path + "richards.js"); // not used because not memory heavy
-  if (!single) {
-    jetstream2_js_paths.push_back({octane_path, "earley-boyer.js"}); // comment out temporarily
-  }
+  jetstream2_js_paths.push_back({octane_path, "earley-boyer.js", 3000});
   // js_paths.push_back(octane_path + "deltablue.js"); // not used because not memory heavy
-  if (!single) {
-    jetstream2_js_paths.push_back({octane_path, "pdfjs.js"}); // comment out temporarily
-  }
-  jetstream2_js_paths.push_back({octane_path, "splay.js"}); // comment out temporarily
-  if (!single) {
-    jetstream2_js_paths.push_back({jetstream2_path, "simple/hash-map.js"}); // comment out temporarily
-  }
+  jetstream2_js_paths.push_back({octane_path, "pdfjs.js", 1000});
+  jetstream2_js_paths.push_back({octane_path, "splay.js", 1000});
+  jetstream2_js_paths.push_back({jetstream2_path, "simple/hash-map.js", 3000});
   // jetstream2_js_paths.push_back({octane_path, "box2d.js"}); // not used because not memory heavy
   // jetstream2_js_paths.push_back({jetstream2_path, "Seamonster/gaussian-blur.js"}); // not used because not memory heavy
-  std::vector<std::pair<std::string, std::string>> js_paths;
-  if (!single) {
-    js_paths.push_back({sunspider_path, "tagcloud.js"}); // new benchmark; comment out temporarily
-  }
+  std::vector<Benchmark> js_paths;
+  js_paths.push_back({sunspider_path, "tagcloud.js", 3000});
   Signal s;
   std::vector<std::thread> threads;
   std::vector<std::future<V8_Result>> futures;
   {
     std::string header = "let performance = {now() { return 0; }};";
-    std::string footer = "for(i = 0; i < 1000; i++) {new Benchmark().runIteration();}";
-    for (const std::pair<std::string, std::string>& p : jetstream2_js_paths) {
-      std::string js_directory = p.first;
-      std::string js_name = p.second;
-      std::string js_path = js_directory + js_name;
+    for (const Benchmark&b : jetstream2_js_paths) {
+      std::string js_path = b.directory + b.name;
+      std::string footer = "for(i = 0; i < " + std::to_string(b.repeat_time) + "; i++) {new Benchmark().runIteration();}";
       Signal* ps = &s;
       std::vector<std::pair<size_t, std::string>> input = {{1, header}, {1, read_file(js_path)}, {1, footer}};
-      futures.push_back(std::async(std::launch::async, run_v8, platform, input, js_name, heap_size, ps));
+      futures.push_back(std::async(std::launch::async, run_v8, platform, input, b.name, heap_size, ps));
     }
   }
 
@@ -125,13 +121,11 @@ void v8_experiment(v8::Platform* platform, const std::vector<char*>& args) {
     }
   }
 
-  for (const std::pair<std::string, std::string>& p : js_paths) {
-    std::string js_directory = p.first;
-    std::string js_name = p.second;
-    std::string js_path = js_directory + js_name;
+  for (const Benchmark& b : js_paths) {
+    std::string js_path = b.directory + b.name;
     Signal* ps = &s;
-    std::vector<std::pair<size_t, std::string>> input = {{1, std::string("for(i = 0; i < 1000; i++) {") + read_file(js_path) + "}"}};
-    futures.push_back(std::async(std::launch::async, run_v8, platform, input, js_name, heap_size, ps));
+    std::vector<std::pair<size_t, std::string>> input = {{1, std::string("for(i = 0; i < " + std::to_string(b.repeat_time) + "; i++) {") + read_file(js_path) + "}"}};
+    futures.push_back(std::async(std::launch::async, run_v8, platform, input, b.name, heap_size, ps));
   }
 
   s.signal();
