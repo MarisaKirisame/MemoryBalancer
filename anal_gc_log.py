26650730207879b86aaf48bb6141ca43443732df
Pranav Shenoy <pranav.shenoy@utah.edu>
2022-02-20 14:17:16 -0700

    vertical lines

--- anal_gc_log.py
+++ anal_gc_log.py
@@ -16,30 +16,34 @@ def stack(l, r):
     len_r = len(r)
     old_lx = 0
     old_ly = 0
+    old_lz = 0
     old_rx = 0
     old_ry = 0
+    old_rz = 0
     ret = []
     while True:
         if len_l == 0:
-            return ret + [(x, y + old_ly) for x, y in r]
+            return ret + [(x, y + old_ly, z) for x, y, z in r]
         elif len_r == 0:
-            return ret + [(x, y + old_ry) for x, y in l]
+            return ret + [(x, y + old_ry, z) for x, y, z in l]
         else:
             assert len_l > 0 and len_r > 0
-            (lx, ly) = l[0]
-            (rx, ry) = r[0]
+            (lx, ly, lz) = l[0]
+            (rx, ry, rz) = r[0]
             if lx < rx:
                 l = l[1:]
                 len_l -= 1
                 old_lx = lx
                 old_ly = ly
-                ret.append((lx, old_ly + interpol((old_rx, old_ry), (rx, ry), lx)))
+                old_lz = lz
+                ret.append((lx, old_ly + interpol((old_rx, old_ry), (rx, ry), lx), lz))
             else:
                 r = r[1:]
                 len_r -= 1
                 old_rx = rx
                 old_ry = ry
-                ret.append((rx, interpol((old_lx, old_ly), (lx, ly), rx) + old_ry))
+                old_rz = rz
+                ret.append((rx, interpol((old_lx, old_ly), (lx, ly), rx) + old_ry, rz))
 
 class Stackable:
     def draw(self, baseline):
@@ -61,15 +65,24 @@ class Process(Stackable):
         self.name = name
         self.memory = []
         self.working_memory = []
-    def point(self, time, working_memory, memory):
-        self.memory.append((time, memory))
-        self.working_memory.append((time, working_memory))
+
+    def point(self, time, working_memory, memory, gc_trigger):
+        self.memory.append((time, memory, gc_trigger))
+        self.working_memory.append((time, working_memory, gc_trigger))
+
     def draw(self, baseline):
         memory = stack(baseline, self.memory)
-        p = plt.plot([x for x, y in memory], [y for x, y in memory], label=self.name)
+        p = plt.plot([x for x, y, z in memory], [y for x, y, z in memory], label=self.name)
         working_memory = stack(baseline, self.working_memory)
-        plt.fill_between([x for x, y in working_memory], [y for x, y in working_memory],  [y for x, y in stack(baseline, [(x, 0) for x, y in self.memory])], color=p[0].get_color())
-        plt.plot([x for x, y in working_memory], [y for x, y in working_memory], color=p[0].get_color())
+        plt.fill_between([x for x, y, z in working_memory], [y for x, y, z in working_memory],  [y for x, y, z in stack(baseline, [(x, 0, z) for x, y, z in self.memory])], color=p[0].get_color())
+        plt.plot([x for x, y, z in working_memory], [y for x, y, z in working_memory], color=p[0].get_color())
+
+        for (index, obj) in enumerate(memory):
+            (time, mem, is_trigger) = obj
+            (work_t, work_mem, is_trigger) = working_memory[index]
+            if is_trigger:
+                plt.vlines(time, ymin=work_mem, ymax=mem, color='black', linestyle= '--')
+
     def stack(self, baseline):
         return stack(baseline, self.memory)
 
@@ -107,9 +120,12 @@ def main(directory):
             x = Process(name)
             instance_map[name] = x
             instance_list.append(x)
-        instance_map[name].point(time, working_memory, memory)
-    draw_stacks(instance_list)
+        gc_trigger = False
+        if l["msg-type"] == "major_gc":
+            gc_trigger = True
+        instance_map[name].point(time, working_memory, memory, gc_trigger)
 
+    draw_stacks(instance_list)
     plt.legend()
     plt.title(title)
 
