e82eecd61e86b296f40e5fbcd519aa3b1fb89082
Pranav Shenoy <pranav.shenoy@utah.edu>
2022-03-18 17:34:15 -0600

    update in upload.sh

--- python/upload.sh
+++ python/upload.sh
@@ -5,7 +5,8 @@ DIR=$1
 TIME=$(date +%s)
 
 last=`ls "$1" | sort -r | head -1`
-python3 python/plot.py "$1/$last"
+./clean_log
+python3 python/gen.py --no-open
 scp plot.png "uwplse.org:/var/www/membalancer/$TIME.png"
 
 if command -v nightly-results &>/dev/null; then
