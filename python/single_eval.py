08823c17c536c1c03cf0ba654ab29aaf586794d3
Pranav Shenoy <pranav.shenoy@utah.edu>
2022-03-22 16:09:45 -0600

    intendation

--- python/single_eval.py
+++ python/single_eval.py
@@ -179,11 +179,12 @@ def run_jetstream(v8_env_vars):
         j["OK"] = True
         j["MAJOR_GC_TIME"] = calculate_total_major_gc_time(result_directory)
         for p in ["PhysicalMemory", "SizeOfObjects", "Limit"]:
-            j[f"Peak({p})"] = calculate_peak(result_directory, p)
-            j[f"Average({p})"] = calculate_average(result_directory, p)
+        	j[f"Peak({p})"] = calculate_peak(result_directory, p)
+        	j[f"Average({p})"] = calculate_average(result_directory, p)
         j["Peak(BalancerMemory)"] = calculate_peak_balancer_memory(result_directory)
         j["Average(BalancerMemory)"] = calculate_peak_balancer_memory(result_directory)
         v8_log_path = os.path.join(result_directory, "v8_log")
+        
         
         total_time = None
         total_major_gc_time = None
