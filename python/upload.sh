30e12dd6b50bf832ab0098ff02f3c25fcbfadc49
Pranav Shenoy <pranav.shenoy@utah.edu>
2022-03-23 16:54:57 -0600

    clean log

--- python/upload.sh
+++ python/upload.sh
@@ -1,16 +1,9 @@
 #!/bin/sh
 set -o errexit -o xtrace
 
-# DIR=$1
-# TIME=$(date +%s)
-
-#./clean_log
-
 python3 python/gen.py --no-open
 last=`ls "out" | sort -r | head -1`
 result_dir="out/$last"
-#echo "**creating $last directory"
-#ssh "uwplse.org" "mkdir /var/www/membalancer/$last"
 echo "** uploading files to membalancer.uwplse.org/$last **"
 scp -r $result_dir "uwplse.org:/var/www/membalancer"
 echo "** uploaded files **"
