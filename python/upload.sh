aa406996b6590a54bd2e133e1ed1487b56169bc1
Pranav Shenoy <pranav.shenoy@utah.edu>
2022-03-23 13:25:32 -0600

    upload feature

--- python/upload.sh
+++ python/upload.sh
@@ -1,16 +1,26 @@
 #!/bin/sh
 set -o errexit -o xtrace
 
-DIR=$1
-TIME=$(date +%s)
+# DIR=$1
+# TIME=$(date +%s)
 
-last=`ls "$1" | sort -r | head -1`
 #./clean_log
 
-
 python3 python/gen.py --no-open
-scp plot.png "uwplse.org:/var/www/membalancer/$TIME.png"
+last=`ls "$1" | sort -r | head -1`
+result_dir="out/$last"
+echo "**creating $last directory"
+ssh "uwplse.org" "mkdir /var/www/membalancer/$last"
+echo "**uploading files to membalancer.uwplse.org/$last **"
+scp -r result_dir "uwplse.org:/var/www/membalancer/$last"
 
-if command -v nightly-results &>/dev/null; then
-    nightly-results img https://membalancer.uwplse.org/$TIME.png
-fi
+
+
+
+
+
+# scp plot.png "uwplse.org:/var/www/membalancer/$TIME.png"
+# 
+# if command -v nightly-results &>/dev/null; then
+#     nightly-results img https://membalancer.uwplse.org/$TIME.png
+# fi
