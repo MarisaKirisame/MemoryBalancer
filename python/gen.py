2986452e2ddda1bb41fb658d71ff0b6cae37b8d7
Pranav Shenoy <pranav.shenoy@utah.edu>
2022-04-12 01:30:24 -0600

    mnr

--- python/gen.py
+++ python/gen.py
@@ -189,18 +189,18 @@ if eval_name == "JS":
             	tex_table_baseline_dir = dirname
             	found_baseline = True
             	anal_gc_log.main(dirname + "/", legend=False)
-                plt.xlim([0,30])
-                plt.ylim([0,400])
-                plt.savefig(f"../membalancer-paper/js_baseline_anal.png", bbox_inches='tight')
-                plt.clf()
+            	plt.xlim([0,30])
+            	plt.ylim([0,400])
+            	plt.savefig(f"../membalancer-paper/js_baseline_anal.png", bbox_inches='tight')
+            	plt.clf()
         elif cfg["BALANCER_CFG"]["RESIZE_CFG"]["GC_RATE_D"] == -5e-10:
         	tex_table_membalancer_dir = dirname
         	anal_gc_log.main(dirname + "/", legend=False)
-            plt.xlim([0, 30])
-            plt.ylim([0, 400])
-            plt.savefig(f"../membalancer-paper/js_membalancer_anal.png", bbox_inches='tight')
-            plt.clf()
-    
+        	plt.xlim([0, 30])
+        	plt.ylim([0, 400])
+        	plt.savefig(f"../membalancer-paper/js_membalancer_anal.png", bbox_inches='tight')
+        	plt.clf()
+        	
     gen_tex_table.main(tex_table_membalancer_dir, tex_table_baseline_dir)
 for name in glob.glob('log/**/score', recursive=True):
     pass # todo: write commit upload
