ff000538ba52182575daee105d4fcb51c502c77c
Marisa Kirisame <lolisa@marisa.moe>
2022-04-13 18:50:38 -0600

    save

--- python/eval.py
+++ python/eval.py
@@ -175,7 +175,7 @@ BASELINE = {
 BALANCER_CFG = QUOTE(NONDET(BASELINE, BASELINE, BASELINE,
                             {
                                 "BALANCE_STRATEGY": "classic",
-                                "RESIZE_CFG": {"RESIZE_STRATEGY": "gradient", "GC_RATE_D":NONDET(*[x / -1e9for x in [0.01, 0.02, 0.03, 0.04, 0.05, 0.1, 0.2, 0.3, 0.5, 0.7, 0.9]])},
+                                "RESIZE_CFG": {"RESIZE_STRATEGY": "gradient", "GC_RATE_D":NONDET(*[x / -1e9for x in [0.01, 0.02, 0.03, 0.04, 0.05, 0.1, 0.2, 0.3, 0.5, 0.7, 0.9, 2, 3, 4]])},
                                 "BALANCE_FREQUENCY": 0
                             }))
 
