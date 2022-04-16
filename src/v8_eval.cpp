0dee280afdc9c13c8c69d7c869b68d0db2af215e
Marisa Kirisame <lolisa@marisa.moe>
2022-04-15 22:56:35 -0600

    save

--- src/v8_eval.cpp
+++ src/v8_eval.cpp
@@ -83,7 +83,7 @@ void v8_experiment(v8::Platform* platform, const std::vector<char*>& args) {
   std::string sunspider_path = jetstream1_path + "sunspider/";
   std::vector<Benchmark> jetstream2_js_paths;
   std::vector<Benchmark> js_paths;
-  jetstream2_js_paths.push_back({octane_path, "splay.js", 1000});
+  jetstream2_js_paths.push_back({octane_path, "splay.js", 1500});
   jetstream2_js_paths.push_back({octane_path, "pdfjs.js", 1000});
   Signal s;
   std::vector<std::thread> threads;
