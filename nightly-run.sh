c887c583a987181d8471af169e99c0b97ae8d2e8
Marisa Kirisame <lolisa@marisa.moe>
2022-06-09 22:58:34 -0600

    save

--- nightly-run.sh
+++ nightly-run.sh
@@ -57,5 +57,4 @@ last=`ls "out" | sort -r | head -1`
 result_dir="out/$last"
 if command -v nightly-results &>/dev/null; then
     nightly-results url "http://membalancer.uwplse.org/$last"
-    nightly-results img "http://membalancer.uwplse.org/$last/plot.png"
 fi
