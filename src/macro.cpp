8a3f2f8f4ffd5f2e8b0ff1f61d5c5e6858355d21
霧雨魔理沙 <lolisa@marisa.moe>
2022-07-27 10:43:02 -0600

    Update macro.cpp

--- src/macro.cpp
+++ src/macro.cpp
@@ -14,7 +14,7 @@ void macro() {
   nlohmann::json j;
   j["MinHeapExtraSizeInMB"] = v8::Isolate::MinHeapExtraSizeInMB();
   j["BiasInWorkingMemoryInMB"] = bytes_to_mb(bias_in_working_memory);
-  j["InitialGCSpeed"] = initial_garbage_bytes / initial_garbage_duration * 1000;
+  j["InitialGCSpeedInMBPerSec"] = initial_garbage_bytes / initial_garbage_duration * 1000;
   j["GarbageRateDecayPerSec"] = garbage_rate_decay_per_sec;
   j["GCSpeedSmoothingPerSample"] = gc_speed_smoothing_per_sample;
   j["TotalMemoryFloorInMB"] = bytes_to_mb(total_memory_floor);
