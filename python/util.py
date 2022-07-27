0a9d4094284b220ed5f734b93290b15f5c0cc98e
Marisa Kirisame <lolisa@marisa.moe>
2022-07-26 20:43:32 -0600

    save

--- python/util.py
+++ python/util.py
@@ -3,7 +3,7 @@ from pyppeteer import launch
 import collections
 
 def fmt(x):
-	return "{0:.2g}".format(float(x))
+	return "%s".format(float("{0:.2g}".format(float(x))))
 
 def fmt_int(x):
 	return "{}".format(int(x))
