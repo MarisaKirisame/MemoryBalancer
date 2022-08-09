5a5a370f58f34b1217e4b6b30f5e294680db659f
Marisa Kirisame <lolisa@marisa.moe>
2022-08-09 13:23:20 -0600

    save

--- python/anal_gc_log.py
+++ python/anal_gc_log.py
@@ -150,23 +150,24 @@ def main(cfg, exp, legend=True):
                     j = json.loads(line)
                     j["type"] = "memory"
                     jsons.append(j)
-            jsons.sort(key=lambda x:x["time"])
-            x = Process(name)
-            instance_list.append(x)
-            working_memory = 0
-            for j in jsons:
-                if j["type"] == "gc" and not j["major"]:
-                    pass
-                elif j["type"] == "memory":
-                    current_memory = j["AllocatedExternalMemorySinceMarkCompact"] + j["SizeOfObjects"]
-                    max_memory = j["Limit"]
-                    x.point(j["time"] / 1e9, working_memory / 1e6, current_memory / 1e6, max_memory / 1e6, False)
-                else:
-                    working_memory = j["after_memory"]
-                    current_memory = working_memory
-                    max_memory = j["Limit"]
-                    x.point(j["time"] / 1e9, working_memory / 1e6, current_memory / 1e6, max_memory / 1e6, True)
-            x.point((j["time"] + 1) / 1e9, 0, 0, 0, False)
+            if len(jsons) != 0:
+                jsons.sort(key=lambda x:x["time"])
+                x = Process(name)
+                instance_list.append(x)
+                working_memory = 0
+                for j in jsons:
+                    if j["type"] == "gc" and not j["major"]:
+                        pass
+                    elif j["type"] == "memory":
+                        current_memory = j["AllocatedExternalMemorySinceMarkCompact"] + j["SizeOfObjects"]
+                        max_memory = j["Limit"]
+                        x.point(j["time"] / 1e9, working_memory / 1e6, current_memory / 1e6, max_memory / 1e6, False)
+                    else:
+                        working_memory = j["after_memory"]
+                        current_memory = working_memory
+                        max_memory = j["Limit"]
+                        x.point(j["time"] / 1e9, working_memory / 1e6, current_memory / 1e6, max_memory / 1e6, True)
+                x.point((j["time"] + 1) / 1e9, 0, 0, 0, False)
     instance_list.sort(key=lambda x: x.name)
     draw_stacks(instance_list)
     if legend:
