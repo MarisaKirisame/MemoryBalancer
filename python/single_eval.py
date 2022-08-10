d28800dda371e53d39d2219c31d6ba31c8fb6b3c
Marisa Kirisame <lolisa@marisa.moe>
2022-08-10 16:21:22 -0600

    save

--- python/single_eval.py
+++ python/single_eval.py
@@ -255,7 +255,7 @@ def run_browser(v8_env_vars):
         return bench[bench_name]
 
     async def run_browser_main():
-        b = await new_browser(env_vars=v8_env_vars, headless=True, debug=False)
+        b = await new_browser(env_vars=v8_env_vars, headless=False, debug=False)
         d = 180
         try:
             await asyncio.wait_for(asyncio.gather(*[get_bench(bench)(b, d) for bench in BENCH]), timeout=d*2)
